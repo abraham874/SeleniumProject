06a64fb36110ca1d5ab6c32205fdb3b6a5700510
abraham874 <157042825+abraham874@users.noreply.github.com>
2024-01-24 10:16:46 +0530

    Updated ElementLocators.java

--- ElementLocators.java
+++ ElementLocators.java
@@ -1,6 +1,9 @@
 package project;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,17 +14,24 @@ public class ElementLocators {
 		System.setProperty("webdriver.chrome.driver","./Driver01/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 
-		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.get("https://google.com/");
 		driver.manage().window().maximize();
 		
-		driver.findElement(By.xpath("//input[@id='name']")).sendKeys("student");
-		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("stduent123@gmail.com");
-		driver.findElement(By.xpath("//input[@id='phone']")).sendKeys("8523697412");
-		driver.findElement(By.xpath("//textarea[@id='textarea']")).sendKeys("No 156/87, Nasvhville, USA");
 		
-		WebElement radio1 = driver.findElement(By.xpath("//input[@id='male']"));
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);	
 		
-		radio1.click();
+		driver.findElement(By.xpath("//textarea[@title='Search']")).sendKeys("amazon registration");
+		driver.findElement(By.xpath("//textarea[@title='Search']")).sendKeys(Keys.RETURN);
+		
+		driver.findElement(By.xpath("//a[@href='https://www.amazon.in/ap/register?openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fref%3Drhf_sign_in&openid.assoc_handle=inflex']")).click();
+		
+		//locating the textboxes
+		
+		driver.findElement(By.xpath("//input[@id='ap_customer_name']")).sendKeys("student");
+		driver.findElement(By.xpath("//input[@id='ap_phone_number']")).sendKeys("8654123788");
+		driver.findElement(By.xpath("//input[@id='ap_email']")).sendKeys("student123@gmail.com");
+		driver.findElement(By.xpath("//input[@id='ap_password']")).sendKeys("123456789hello");
+		
 		
 		
 		
